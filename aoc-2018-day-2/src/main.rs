c5f3f4947f3e5a5b609aa1d35d2dfb641d542ffb
ptillemans <pti@snamellit.com>
2022-11-10 20:00:04 +0100

    add day 2 of 2018

---
+++ aoc-2018-day-2/src/main.rs
@@ -0,0 +1,76 @@
+use std::str::FromStr;
+
+const INPUT: &str = include_str!("../data/input.txt");
+
+
+#[derive(Debug, PartialEq, Eq)]
+struct InputModel  {
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum AocError {
+    #[error("Error parsing the input")]
+    ParseError,
+}
+        
+impl FromStr for InputModel {
+    type Err = AocError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        todo!()
+    }
+}
+
+fn part1(_input: &InputModel) -> Result<String,AocError> {
+    return Ok("Not implemented".to_string())
+}
+
+fn part2(_input: &InputModel) -> Result<String, AocError> {
+    return Ok("Not implemented".to_string())
+}
+
+fn main() -> Result<(), AocError> {
+    let input:InputModel = INPUT.parse::<InputModel>()?;
+    let part1_result = part1(&input)?;
+    println!("Part1: {}", part1_result);
+    println!("--------------");
+    let part2_result = part2(&input)?;
+    println!("Part2: {}", part2_result);
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST_INPUT: &str = "";
+
+    fn input_data() -> InputModel {
+        InputModel {
+        }
+    }
+
+    #[test]
+    fn test_parse() {
+        let actual = TEST_INPUT.parse::<InputModel>().unwrap();
+        let expected = input_data();
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_part1() {
+        let actual = part1(&input_data()).unwrap();
+        let expected = "";
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_part2() {
+        let actual = part2(&input_data()).unwrap();
+        let expected = "";
+
+        assert_eq!(actual, expected);
+    }
+}
