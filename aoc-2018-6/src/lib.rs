5a2eed05f224683c338b1de90977d2ad79cdb9fb
ptillemans <pti@snamellit.com>
2022-11-17 08:56:47 +0100

    refactor as workspace

---
+++ aoc-2018-6/src/lib.rs
@@ -0,0 +1,26 @@
+use std::str::FromStr;
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct InputModel  {
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum AocError {
+    #[error("Error parsing the input")]
+    ParseError,
+}
+        
+impl FromStr for InputModel {
+    type Err = AocError;
+
+    fn from_str(_s: &str) -> Result<Self, Self::Err> {
+        todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+
+}
