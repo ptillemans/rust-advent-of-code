9ee1c7c77b951a7ffb405d1c049d89c4f5a3ed88
ptillemans <pti@snamellit.com>
2022-11-21 23:31:57 +0100

    parsing implemented

--- aoc-2018-6/src/lib.rs
+++ aoc-2018-6/src/lib.rs
@@ -1,7 +1,11 @@
 use std::str::FromStr;
 
+// solve day 6 of AdventOfCode
+//
+
 #[derive(Debug, PartialEq, Eq)]
 pub struct InputModel  {
+    pub points: Vec<(i32, i32)>,
 }
 
 #[derive(thiserror::Error, Debug)]
@@ -9,12 +13,24 @@ pub enum AocError {
     #[error("Error parsing the input")]
     ParseError,
 }
-        
+
+// parse a point from a line
+fn parse_line(s: &str) -> Result<(i32, i32), AocError> {
+    let mut parts = s.split(", ");
+    let x = parts.next().ok_or(AocError::ParseError)?;
+    let y = parts.next().ok_or(AocError::ParseError)?;
+    let x = x.parse::<i32>().map_err(|_| AocError::ParseError)?;
+    let y = y.parse::<i32>().map_err(|_| AocError::ParseError)?;
+    Ok((x, y))
+}
+
+
 impl FromStr for InputModel {
     type Err = AocError;
 
     fn from_str(_s: &str) -> Result<Self, Self::Err> {
-        todo!()
+        let points = _s.lines().map(parse_line).collect::<Result<Vec<_>, _>>()?;
+        Ok(InputModel { points })
     }
 }
 
