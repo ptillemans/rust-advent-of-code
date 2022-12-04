9f1c304a2d5a9e5ec30c7c544d0a932f3fb7ea66
ptillemans <pti@snamellit.com>
2022-12-04 11:15:41 +0100

    solved day 4

---
+++ aoc-2022-4/src/main.rs
@@ -0,0 +1,77 @@
+use aoc_2022_4::{AocError, InputModel, contains, overlap};
+
+const INPUT: &str = include_str!("../data/input.txt");
+
+
+fn part1(input: &InputModel) -> Result<String,AocError> {
+   Ok(input.assignments.iter()
+       .filter(|assignment| contains(assignment.0, assignment.1))
+       .count()
+       .to_string())
+}
+
+fn part2(input: &InputModel) -> Result<String, AocError> {
+   Ok(input.assignments.iter()
+       .filter(|assignment| overlap(assignment.0, assignment.1))
+       .count()
+       .to_string())
+}
+
+fn main() -> Result<(), AocError> {
+    let input:InputModel = INPUT.parse::<InputModel>()?;
+    let part1_result = part1(&input)?;
+    println!("Part1: {}", part1_result);
+    println!("--------------");
+    let part2_result = part2(&input)?;
+    println!("Part2: {}", part2_result);
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST_INPUT: &str = "2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8";
+
+    pub fn input_data() -> InputModel {
+        InputModel {
+            assignments: vec![
+                ((2, 4), (6, 8)),
+                ((2, 3), (4, 5)),
+                ((5, 7), (7, 9)),
+                ((2, 8), (3, 7)),
+                ((6, 6), (4, 6)),
+                ((2, 6), (4, 8)),
+            ],
+        }
+    }
+
+    #[test]
+    fn test_parse() {
+        let actual = TEST_INPUT.parse::<InputModel>().unwrap();
+        let expected = input_data();
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_part1() {
+        let actual = part1(&input_data()).unwrap();
+        let expected = "2";
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_part2() {
+        let actual = part2(&input_data()).unwrap();
+        let expected = "4";
+
+        assert_eq!(actual, expected);
+    }
+}
