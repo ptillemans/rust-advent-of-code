e68b548b56cba6cc014460a38f8a77fcbfd5eb39
ptillemans <pti@snamellit.com>
2022-12-05 20:22:27 +0100

    solved day 5 of 2022

--- aoc-2022-5/src/main.rs
+++ aoc-2022-5/src/main.rs
@@ -1,14 +1,30 @@
-use aoc_2022_5::{AocError, InputModel};
+use aoc_2022_5::{AocError, InputModel, Crate};
 
 const INPUT: &str = include_str!("../data/input.txt");
 
 
-fn part1(_input: &InputModel) -> Result<String,AocError> {
-    return Ok("Not implemented".to_string())
+fn part1(input: &InputModel) -> Result<String,AocError> {
+    let mut stacks = input.stacks.clone();
+    let moves = input.moves.clone();
+    moves.into_iter()
+        .map(|m| stacks.apply_move(m))
+        .collect::<Result<Vec<_>,_>>()?;
+    
+    stacks.heads().iter()
+       .map(|c| c.map(Crate::to_char).ok_or(AocError::NoSolution))
+       .collect()
 }
+    
 
-fn part2(_input: &InputModel) -> Result<String, AocError> {
-    return Ok("Not implemented".to_string())
+fn part2(input: &InputModel) -> Result<String, AocError> {
+    let mut stacks = input.stacks.clone();
+    let moves = input.moves.clone();
+    moves.into_iter()
+        .map(|m| stacks.apply_move_9001(m))
+        .collect::<Result<Vec<_>,_>>()?;
+    stacks.heads().iter()
+       .map(|c| c.map(Crate::to_char).ok_or(AocError::NoSolution))
+       .collect()
 }
 
 fn main() -> Result<(), AocError> {
@@ -23,12 +39,33 @@ fn main() -> Result<(), AocError> {
 
 #[cfg(test)]
 mod tests {
+    use aoc_2022_5::{Moves, Stacks, Move, Stack};
+
     use super::*;
 
-    const TEST_INPUT: &str = "";
+    const TEST_INPUT: &str = "    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2";
 
     pub fn input_data() -> InputModel {
         InputModel {
+            stacks: Stacks::new(vec![
+                Stack::from_chars(vec!['Z', 'N']),
+                Stack::from_chars(vec!['M', 'C', 'D']),
+                Stack::from_chars(vec!['P']),
+            ]),
+            moves: Moves::new(vec![
+                Move{ from: 2, to: 1, amount: 1 },
+                Move{ from: 1, to: 3, amount: 3 },
+                Move{ from: 2, to: 1, amount: 2 },
+                Move{ from: 1, to: 2, amount: 1 },
+            ]),
         }
     }
 
@@ -43,15 +80,15 @@ mod tests {
     #[test]
     fn test_part1() {
         let actual = part1(&input_data()).unwrap();
-        let expected = "";
-
+        let expected = "CMZ".to_string();
+        
         assert_eq!(actual, expected);
     }
 
     #[test]
     fn test_part2() {
         let actual = part2(&input_data()).unwrap();
-        let expected = "";
+        let expected = "MCD";
 
         assert_eq!(actual, expected);
     }
