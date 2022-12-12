d7d8d7e5f21d554126acd34469a11e3ec69a90a0
ptillemans <pti@snamellit.com>
2022-12-12 17:12:51 +0100

    add aoc-common

---
+++ aoc-common/src/position.rs
@@ -0,0 +1,81 @@
+use std::ops::Add;
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
+pub struct Position {
+    pub x: i32, 
+    pub y: i32
+}
+
+impl Add for Position {
+    type Output = Position;
+
+    fn add(self, other: Position) -> Position {
+        (self.x + other.x, self.y + other.y).into()
+    }
+}   
+
+impl <T> From<(T, T)> for Position
+    where T: Into<i32> {
+    fn from((x, y): (T, T)) -> Self {
+        Position { x: x.into(), y: y.into() }
+    }
+}
+
+impl Position {
+
+    // calculate the manhattan distance between two positions
+    pub fn manhattan(&self, other: &Position) -> i32 {
+        (self.x - other.x).abs() + (self.y - other.y).abs()
+    }
+
+    // calculate the chebychev distance between two positions
+    pub fn chebychev(&self, other: &Position) -> i32 {
+        (self.x - other.x).abs().max((self.y - other.y).abs())
+    }
+
+    // calculate the euclidean distance between two positions
+    pub fn distance(&self, other: &Position) -> f32 {
+        ((self.x as f32 - other.x as f32).powf(2.0) 
+            + (self.y as f32 - other.y as f32).powf(2.0)).sqrt()
+    }
+
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+
+    #[test]
+    fn test_add() {
+        let actual = Position { x: 1, y: 2 } + Position { x: 3, y: 4 };
+        let expected = Position { x: 4, y: 6 };
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_into_i32() {
+        let actual: Position = (1, 2).into();
+        let expected = Position { x: 1, y: 2 };
+
+        assert_eq!(actual, expected);
+    }  
+
+    #[test]
+    fn test_into_usize() {
+        let actual: Position = (1, 2).into();
+        let expected = Position { x: 1, y: 2 };
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_manhattan() {
+        let actual = Position { x: 1, y: 2 }.manhattan(&Position { x: 3, y: 4 });
+        let expected = 4;
+
+        assert_eq!(actual, expected);
+    }
+}
