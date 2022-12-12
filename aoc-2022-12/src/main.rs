3543f332176a5472e7f34d4f7497641d2b89718e
ptillemans <pti@snamellit.com>
2022-12-12 12:14:06 +0100

    day 12

--- aoc-2022-12/src/main.rs
+++ aoc-2022-12/src/main.rs
@@ -1,17 +1,19 @@
 #![feature(test)]
-use aoc_2022_11::*;
+use aoc_2022_12::*;
 
 const INPUT: &str = include_str!("../data/input.txt");
 
 
 fn part1(input: &InputModel) -> Result<String,AocError> {
-    let mut troop = MonkeyTroop::new(input.monkeys.clone(), false);
-    Ok(troop.monkey_business(20).to_string())
+    let start = find_start(input);
+    let end = find_end(input);
+    let path = shortest_path(input, &start, &end)?;
+    // steps between start and end is 1 less than the length of the path
+    return Ok((path.len() - 1).to_string())
 }
 
-fn part2(input: &InputModel) -> Result<String, AocError> {
-    let mut troop = MonkeyTroop::new(input.monkeys.clone(), true);
-    Ok(troop.monkey_business(10000).to_string())
+fn part2(_input: &InputModel) -> Result<String, AocError> {
+    return Ok("Not implemented".to_string())
 }
 
 fn main() -> Result<(), AocError> {
@@ -19,7 +21,6 @@ fn main() -> Result<(), AocError> {
     let part1_result = part1(&input)?;
     println!("Part1: {}", part1_result);
     println!("--------------");
-    let input:InputModel = INPUT.parse::<InputModel>()?;
     let part2_result = part2(&input)?;
     println!("Part2: {}", part2_result);
     Ok(())
@@ -31,34 +32,15 @@ mod tests {
     use super::*;
     use test::Bencher;
 
-    const TEST_INPUT: &str = "Monkey 0:
-  Starting items: 79, 98
-  Operation: new = old * 19
-  Test: divisible by 23
-    If true: throw to monkey 2
-    If false: throw to monkey 3
+    const TEST_INPUT: &str = "Sabqponm
+abcryxxl
+accszExk
+acctuvwj
+abdefghi";
 
-Monkey 1:
-  Starting items: 54, 65, 75, 74
-  Operation: new = old + 6
-  Test: divisible by 19
-    If true: throw to monkey 2
-    If false: throw to monkey 0
-
-Monkey 2:
-  Starting items: 79, 60, 97
-  Operation: new = old * old
-  Test: divisible by 13
-    If true: throw to monkey 1
-    If false: throw to monkey 3
-
-Monkey 3:
-  Starting items: 74
-  Operation: new = old + 3
-  Test: divisible by 17
-    If true: throw to monkey 0
-    If false: throw to monkey 1";
-
+    pub fn input_data() -> InputModel {
+        test_data()
+    }
 
     #[test]
     fn test_parse() {
@@ -71,7 +53,7 @@ Monkey 3:
     #[test]
     fn test_part1() {
         let actual = part1(&input_data()).unwrap();
-        let expected = "10605";
+        let expected = "31";
 
         assert_eq!(actual, expected);
     }
@@ -79,7 +61,7 @@ Monkey 3:
     #[test]
     fn test_part2() {
         let actual = part2(&input_data()).unwrap();
-        let expected = "2713310158";
+        let expected = "";
 
         assert_eq!(actual, expected);
     }
