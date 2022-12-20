f71545e0fa454ecd1439c0fd8f70228f1def40f1
ptillemans <pti@snamellit.com>
2022-12-21 00:01:57 +0100

    solved part 2

--- aoc-2022-20/src/lib.rs
+++ aoc-2022-20/src/lib.rs
@@ -2,7 +2,7 @@ use std::str::FromStr;
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct InputModel {
-    pub coordinates: Vec<i32>,
+    pub coordinates: Vec<i64>,
 }
 
 #[derive(thiserror::Error, Debug)]
@@ -16,7 +16,7 @@ impl FromStr for InputModel {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         s.lines()
-            .map(|line| line.parse::<i32>())
+            .map(|line| line.parse::<i64>())
             .collect::<Result<Vec<_>, _>>()
             .map(|coordinates| InputModel { coordinates })
             .map_err(|_| AocError::ParseError)
@@ -25,12 +25,12 @@ impl FromStr for InputModel {
 
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub struct CodeRing {
-    code: Vec<(usize, i32)>,
+    code: Vec<(usize, i64)>,
 }
 
 impl CodeRing {
    
-    pub fn new(code: Vec<i32>) -> Self {
+    pub fn new(code: Vec<i64>) -> Self {
         let code = code
             .into_iter()
             .enumerate()
@@ -38,11 +38,11 @@ impl CodeRing {
         CodeRing { code }
     }
 
-    pub fn decode(&mut self) -> i32 {
+    pub fn decode(&mut self) -> i64 {
         let original = self.code.clone();
         for (orig_pos, value) in original.iter() {
             let from_idx = self.code.iter().position(|(pos, _)| pos == orig_pos).unwrap();
-            let to_idx = ((from_idx as i32 + value).rem_euclid((self.code.len() - 1) as i32)) as usize;
+            let to_idx = ((from_idx as i64 + value).rem_euclid((self.code.len() - 1) as i64)) as usize;
             let t = self.code.remove(from_idx);
             self.code.insert(to_idx, t);
         };
@@ -51,6 +51,30 @@ impl CodeRing {
         (1000..=3000).step_by(1000)
             .map(|i| (i + offset) % self.code.len())
             .map(|i| self.code[i].1)
+            .sum()
+        
+    }
+
+    pub fn decode_2(&mut self) -> i64 {
+        let magic = 811589153;
+        let original = self.code.clone()
+            .into_iter()
+            .map(|(pos, value)| (pos, value * magic))
+            .collect::<Vec<_>>();
+        self.code = original.clone();
+        for _ in 0..10 {
+            for (orig_pos, value) in original.iter() {
+                let from_idx = self.code.iter().position(|(pos, _)| pos == orig_pos).unwrap();
+                let to_idx = ((from_idx as i64 + value).rem_euclid((self.code.len() - 1) as i64)) as usize;
+                let t = self.code.remove(from_idx);
+                self.code.insert(to_idx, t);
+            };
+        };
+        let offset = self.code.iter().position(|(_, v)| *v == 0).unwrap();
+        
+        (1000..=3000).step_by(1000)
+            .map(|i| (i + offset) % self.code.len())
+            .map(|i| self.code[i].1 )
             .sum()
         
     }
@@ -85,17 +109,6 @@ pub fn input_data() -> InputModel {
 #[cfg(test)]
 mod tests {
 
-
-    const SEQUENCE: [[i32; 7]; 7] = [
-        [2, 1, -3, 3, -2, 0, 4],
-        [1, -3, 2, 3, -2, 0, 4],
-        [1, 2, 3, -2, -3, 0, 4],
-        [1, 2, -2, -3, 0, 3, 4],
-        [1, 2, -3, 0, 3, 4, -2],
-        [1, 2, -3, 0, 3, 4, -2],
-        [1, 2, -3, 4, 0, 3, -2],
-    ];
-
     #[test]
     fn test_parse() {
         let actual = super::TEST_INPUT.parse::<super::InputModel>().unwrap();
@@ -104,11 +117,17 @@ mod tests {
         assert_eq!(actual, expected);
     }
 
-    //#[test]
-    fn test_sum_coordinates() {
+    #[test]
+    fn test_decode() {
         let input = super::input_data();
         let mut code_ring = super::CodeRing::new(input.coordinates);
-        assert_eq!(code_ring.decode(), 2);
+        assert_eq!(code_ring.decode(), 3);
+    }
+    #[test]
+    fn test_decode_2() {
+        let input = super::input_data();
+        let mut code_ring = super::CodeRing::new(input.coordinates);
+        assert_eq!(code_ring.decode_2(), 1623178306);
     }
 
 }
