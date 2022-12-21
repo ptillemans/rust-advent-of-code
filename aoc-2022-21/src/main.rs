dd2a7cd30ce0692c6301285899dcd6167493742f
ptillemans <pti@snamellit.com>
2022-12-21 23:07:28 +0100

    solved part 2 day 21

--- aoc-2022-21/src/main.rs
+++ aoc-2022-21/src/main.rs
@@ -1,24 +1,31 @@
 #![feature(test)]
-use aoc_2022_21::{AocError, InputModel};
+use aoc_2022_21::*;
 
 const INPUT: &str = include_str!("../data/input.txt");
 
 
-fn part1(_input: &InputModel) -> Result<String,AocError> {
-    return Ok("Not implemented".to_string())
+fn part1(input: &InputModel) -> Result<String,AocError> {
+    let monkey = MonkeyId::new("root");
+    Ok(evaluate(&monkey, &input.expressions)
+       .expect("expression to be valid")
+       .to_string())
 }
 
-fn part2(_input: &InputModel) -> Result<String, AocError> {
-    return Ok("Not implemented".to_string())
+fn part2(input: &InputModel) -> Result<String, AocError> {
+    let expressions = prepare_part2(&input.expressions);
+    let monkey = MonkeyId::new("root");
+    Ok(find_hole(&monkey, 0, &expressions)
+       .expect("expression to be valid")
+       .to_string())
 }
 
 fn main() -> Result<(), AocError> {
     let input:InputModel = INPUT.parse::<InputModel>()?;
     let part1_result = part1(&input)?;
-    println!("Part1: {}", part1_result);
+    println!("Part1: {part1_result}");
     println!("--------------");
     let part2_result = part2(&input)?;
-    println!("Part2: {}", part2_result);
+    println!("Part2: {part2_result}");
     Ok(())
 }
 
@@ -28,11 +35,24 @@ mod tests {
     use super::*;
     use test::Bencher;
 
-    const TEST_INPUT: &str = "";
+    const TEST_INPUT: &str = "root: pppw + sjmn
+dbpl: 5
+cczh: sllz + lgvd
+zczc: 2
+ptdq: humn - dvpt
+dvpt: 3
+lfqf: 4
+humn: 5
+ljgn: 2
+sjmn: drzm * dbpl
+sllz: 4
+pppw: cczh / lfqf
+lgvd: ljgn * ptdq
+drzm: hmdt - zczc
+hmdt: 32"; 
 
     pub fn input_data() -> InputModel {
-        InputModel {
-        }
+        TEST_INPUT.parse::<InputModel>().unwrap()
     }
 
     #[test]
@@ -46,7 +66,7 @@ mod tests {
     #[test]
     fn test_part1() {
         let actual = part1(&input_data()).unwrap();
-        let expected = "";
+        let expected = "152";
 
         assert_eq!(actual, expected);
     }
@@ -54,7 +74,7 @@ mod tests {
     #[test]
     fn test_part2() {
         let actual = part2(&input_data()).unwrap();
-        let expected = "";
+        let expected = "301";
 
         assert_eq!(actual, expected);
     }
