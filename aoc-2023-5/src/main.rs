7fab20931a828b04c1ae9b752a62202ad748a221
Peter Tillemans <pti@melexis.com>
2023-12-05 06:28:20 +0100

    started day 5

---
+++ aoc-2023-5/src/main.rs
@@ -0,0 +1,111 @@
+#![feature(test)]
+use aoc_2023_5::{AocError, InputModel};
+
+const INPUT: &str = include_str!("../data/input.txt");
+
+
+fn part1(_input: &InputModel) -> Result<String,AocError> {
+    return Ok("Not implemented".to_string())
+}
+
+fn part2(_input: &InputModel) -> Result<String, AocError> {
+    return Ok("Not implemented".to_string())
+}
+
+fn main() -> Result<(), AocError> {
+    let input:InputModel = INPUT.parse::<InputModel>()?;
+    let part1_result = part1(&input)?;
+    println!("Part1: {}", part1_result);
+    println!("--------------");
+    let part2_result = part2(&input)?;
+    println!("Part2: {}", part2_result);
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    extern crate test;
+    use super::*;
+    use test::Bencher;
+
+    const TEST_INPUT: &str = "seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4";
+
+    pub fn input_data() -> InputModel {
+        InputModel {
+        }
+    }
+
+    #[test]
+    fn test_parse() {
+        let actual = TEST_INPUT.parse::<InputModel>().unwrap();
+        let expected = input_data();
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_part1() {
+        let actual = part1(&input_data()).unwrap();
+        let expected = "";
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_part2() {
+        let actual = part2(&input_data()).unwrap();
+        let expected = "";
+
+        assert_eq!(actual, expected);
+    }
+    
+    #[bench]
+    fn bench_parse(b: &mut Bencher) {
+        b.iter(|| INPUT.parse::<InputModel>().unwrap())
+    }
+
+    #[bench]
+    fn bench_part1(b: &mut Bencher) {
+        let data = INPUT.parse::<InputModel>().unwrap();
+        b.iter(|| part1(&data))
+    }
+
+    #[bench]
+    fn bench_part2(b: &mut Bencher) {
+        let data = INPUT.parse::<InputModel>().unwrap();
+        b.iter(|| part2(&data))
+    }
+
+}
