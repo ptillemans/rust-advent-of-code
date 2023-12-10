366105b81302064800a43dde5e8c6420f01fa46d
Peter Tillemans <pti@melexis.com>
2023-12-10 09:43:40 +0100

    solved part1 of day 10

--- aoc-2023-10/src/main.rs
+++ aoc-2023-10/src/main.rs
@@ -1,19 +1,46 @@
 #![feature(test)]
-use aoc_2023_10::{AocError, InputModel};
+use aoc_2023_10::{AocError, InputModel, Pipe, Section, find_start};
 
 const INPUT: &str = include_str!("../data/input.txt");
 
+fn part1(input: &InputModel) -> Result<String, AocError> {
+    let pipes = &input.pipes;
+    let start = find_start(&pipes);
+    println!("start {:?}", start);
 
-fn part1(_input: &InputModel) -> Result<String,AocError> {
-    return Ok("Not implemented".to_string())
+    let next_pipes = start.connecting_pipes(pipes);
+    let paths = start
+        .connecting_pipes(pipes)
+        .iter()
+        .filter_map(|pipe| pipe.find_path(pipes, &start))
+        .filter(|path| {
+            matches!(
+                path.last(),
+                Some(Pipe {
+                    loc: _,
+                    section: Section::Start
+                })
+            )
+        })
+        .collect::<Vec<Vec<Pipe>>>();
+
+    println!("{}", paths.len());
+    for path in paths.iter() {
+        println!("{:?}", path);
+    }
+
+    assert!(paths.len() == 2);
+    assert!(paths[0].len() == paths[1].len());
+    let farthest = paths[0].len() / 2;
+    Ok(farthest.to_string())
 }
 
 fn part2(_input: &InputModel) -> Result<String, AocError> {
-    return Ok("Not implemented".to_string())
+    return Ok("Not implemented".to_string());
 }
 
 fn main() -> Result<(), AocError> {
-    let input:InputModel = INPUT.parse::<InputModel>()?;
+    let input: InputModel = INPUT.parse::<InputModel>()?;
     let part1_result = part1(&input)?;
     println!("Part1: {}", part1_result);
     println!("--------------");
@@ -28,11 +55,14 @@ mod tests {
     use super::*;
     use test::Bencher;
 
-    const TEST_INPUT: &str = "";
+    const TEST_INPUT: &str = "-L|F7
+7S-7|
+L|7||
+-L-J|
+L|-JF";
 
     pub fn input_data() -> InputModel {
-        InputModel {
-        }
+        TEST_INPUT.parse::<InputModel>().unwrap()
     }
 
     #[test]
@@ -46,34 +76,30 @@ mod tests {
     #[test]
     fn test_part1() {
         let actual = part1(&input_data()).unwrap();
-        let expected = "";
+        let expected = "4";
 
         assert_eq!(actual, expected);
     }
 
-    #[test]
     fn test_part2() {
         let actual = part2(&input_data()).unwrap();
         let expected = "";
 
         assert_eq!(actual, expected);
     }
-    
+
     #[bench]
     fn bench_parse(b: &mut Bencher) {
         b.iter(|| INPUT.parse::<InputModel>().unwrap())
     }
 
-    #[bench]
     fn bench_part1(b: &mut Bencher) {
         let data = INPUT.parse::<InputModel>().unwrap();
         b.iter(|| part1(&data))
     }
 
-    #[bench]
     fn bench_part2(b: &mut Bencher) {
         let data = INPUT.parse::<InputModel>().unwrap();
         b.iter(|| part2(&data))
     }
-
 }
