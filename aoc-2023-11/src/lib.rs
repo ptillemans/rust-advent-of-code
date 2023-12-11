3177c22a7fd6642acf8c179a1f992ebbadebf129
Peter Tillemans <pti@melexis.com>
2023-12-11 08:43:29 +0100

    solved day 11

---
+++ aoc-2023-11/src/lib.rs
@@ -0,0 +1,173 @@
+use aoc_common::position::Position;
+use std::str::FromStr;
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct InputModel {
+    pub galaxies: Vec<Position>,
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum AocError {
+    #[error("Error parsing the input")]
+    ParseError,
+}
+
+impl FromStr for InputModel {
+    type Err = AocError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let galaxies = s
+            .lines()
+            .enumerate()
+            .flat_map(|(y, line)| {
+                line.chars()
+                    .enumerate()
+                    .filter(|(_, c)| *c == '#')
+                    .map(move |(x, _)| Position::new(x as i32, y as i32))
+            })
+            .collect::<Vec<_>>();
+        Ok(InputModel { galaxies })
+    }
+}
+
+fn empty_rows(input: &InputModel) -> Vec<i32> {
+    let rows: Vec<i32> = input
+        .galaxies
+        .iter()
+        .map(|Position { x: _, y }| *y)
+        .collect::<Vec<_>>();
+
+    let row_max: i32 = rows.iter().cloned().max().unwrap();
+
+    (0..=row_max).filter(|r| !rows.contains(r)).collect()
+}
+
+fn empty_cols(input: &InputModel) -> Vec<i32> {
+    let columns: Vec<i32> = input
+        .galaxies
+        .iter()
+        .map(|Position { x, y: _ }| *x)
+        .collect::<Vec<_>>();
+
+    let column_max: i32 = columns.iter().cloned().max().unwrap();
+
+    (0..=column_max).filter(|r| !columns.contains(r)).collect()
+}
+
+pub type EmptySpace = (Vec<i32>, Vec<i32>);
+
+pub fn empty_space(input: &InputModel) -> EmptySpace {
+    (empty_cols(&input), empty_rows(&input))
+}
+
+pub fn position_after_expansion(pos: &Position, empty_space: &EmptySpace, age: i32) -> Position {
+    let (x, y) = (*pos).into();
+    let (empty_xs, empty_ys) = empty_space;
+
+    let dx = (0..x).filter(|x| empty_xs.contains(x)).count() as i32;
+    let dy = (0..y).filter(|y| empty_ys.contains(y)).count() as i32;
+
+    *pos + (dx * (age - 1), dy * (age - 1)).into()
+}
+
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    const TEST_INPUT: &str = "...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#.....";
+
+    fn input_data() -> InputModel {
+        InputModel {
+            galaxies: vec![
+                Position::new(3, 0),
+                Position::new(7, 1),
+                Position::new(0, 2),
+                Position::new(6, 4),
+                Position::new(1, 5),
+                Position::new(9, 6),
+                Position::new(7, 8),
+                Position::new(0, 9),
+                Position::new(4, 9),
+            ],
+        }
+    }
+
+    #[test]
+    fn test_from_str() {
+        let actual: InputModel = TEST_INPUT.parse().unwrap();
+
+        let expected = input_data();
+        assert_eq!(actual, expected)
+    }
+
+    #[test]
+    fn test_empty_rows() {
+        let input = input_data();
+        let actual = empty_rows(&input);
+        let expected = vec![3, 7];
+
+        assert_eq!(actual, expected)
+    }
+
+    #[test]
+    fn test_empty_cols() {
+        let input = input_data();
+        let actual = empty_cols(&input);
+        let expected = vec![2, 5, 8];
+
+        assert_eq!(actual, expected)
+    }
+
+    #[test]
+    fn test_empty_space() {
+        let input = input_data();
+        let actual = empty_space(&input);
+        let expected = (vec![2, 5, 8], vec![3, 7]);
+
+        assert_eq!(actual, expected)
+    }
+
+    #[test]
+    fn test_position_after_expansion() {
+        let input = input_data();
+        let empty_space = empty_space(&input);
+
+        let test_cases: Vec<((i32, i32), (i32,i32))>= vec![
+            ((0,0), (0,0)),
+            ((6,0), (8,0)),
+            ((0,4), (0,5)),
+            ((9,9), (12,11)),
+        ];
+        for test_case in test_cases {
+            let pos = test_case.0.into(); 
+            let actual = position_after_expansion(&pos, &empty_space, 2);
+            let expected = test_case.1.into();
+            assert_eq!(actual, expected)
+        }
+
+        let test_cases: Vec<((i32, i32), (i32,i32))>= vec![
+            ((0,0), (0,0)),
+            ((6,0), (2000004,0)),
+            ((0,4), (0,1000003)),
+            ((9,9), (3000006,2000007)),
+        ];
+        for test_case in test_cases {
+            let pos = test_case.0.into(); 
+            let actual = position_after_expansion(&pos, &empty_space, 1_000_000);
+            let expected = test_case.1.into();
+            assert_eq!(actual, expected)
+        }
+    }
+
+}
---
+++ aoc-2023-11/src/main.rs
@@ -0,0 +1,117 @@
+#![feature(test)]
+use aoc_2023_11::{AocError, InputModel, position_after_expansion, empty_space};
+use itertools::Itertools;
+
+const INPUT: &str = include_str!("../data/input.txt");
+
+fn part1(input: &InputModel) -> Result<String, AocError> {
+    let sum = sum_combinations(input, 2);
+    Ok(sum.to_string())
+}
+
+fn sum_combinations(input: &InputModel, age: i32) -> u64 {
+    let empty_space = empty_space(&input);
+
+    let new_positions = input.galaxies.iter()
+        .map(|pos| position_after_expansion(pos, &empty_space, age))
+        .collect::<Vec<_>>();
+
+    new_positions.iter()
+        .combinations(2)
+        .map(|ps| ps[0].manhattan(ps[1]) as u64)
+        .sum::<u64>()
+}
+
+fn part2(input: &InputModel) -> Result<String, AocError> {
+    let sum = sum_combinations(input, 1_000_000);
+    Ok(sum.to_string())
+}
+
+fn main() -> Result<(), AocError> {
+    let input: InputModel = INPUT.parse::<InputModel>()?;
+    let part1_result = part1(&input)?;
+    println!("Part1: {}", part1_result);
+    println!("--------------");
+    let part2_result = part2(&input)?;
+    println!("Part2: {}", part2_result);
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    extern crate test;
+    use super::*;
+    use aoc_common::position::Position;
+    use test::Bencher;
+
+    const TEST_INPUT: &str = "...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#.....";
+
+    fn input_data() -> InputModel {
+        InputModel {
+            galaxies: vec![
+                Position::new(3, 0),
+                Position::new(7, 1),
+                Position::new(0, 2),
+                Position::new(6, 4),
+                Position::new(1, 5),
+                Position::new(9, 6),
+                Position::new(7, 8),
+                Position::new(0, 9),
+                Position::new(4, 9),
+            ],
+        }
+    }
+
+    #[test]
+    fn test_parse() {
+        let actual = TEST_INPUT.parse::<InputModel>().unwrap();
+        let expected = input_data();
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_part1() {
+        let actual = part1(&input_data()).unwrap();
+        let expected = "374";
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_sum_combinations() {
+        let input = input_data();
+        let sum_2 = sum_combinations(&input, 2);
+        let sum_10 = sum_combinations(&input, 10);
+        let sum_100 = sum_combinations(&input, 100);
+        assert_eq!(sum_2, 374);
+        assert_eq!(sum_10, 1030);   
+        assert_eq!(sum_100, 8410);   
+    }
+
+    #[bench]
+    fn bench_parse(b: &mut Bencher) {
+        b.iter(|| INPUT.parse::<InputModel>().unwrap())
+    }
+
+    #[bench]
+    fn bench_part1(b: &mut Bencher) {
+        let data = INPUT.parse::<InputModel>().unwrap();
+        b.iter(|| part1(&data))
+    }
+
+    #[bench]
+    fn bench_part2(b: &mut Bencher) {
+        let data = INPUT.parse::<InputModel>().unwrap();
+        b.iter(|| part2(&data))
+    }
+}
