6c421b60dd59a75156faebb4a7fb9716801ad2e5
Peter Tillemans <pti@melexis.com>
2023-12-11 08:43:09 +0100

    used parallellism to speed up day 10

--- aoc-2023-10/src/lib.rs
+++ aoc-2023-10/src/lib.rs
@@ -1,114 +1,133 @@
 use itertools::iterate;
-use std::str::FromStr;
+use std::{collections::HashMap, str::FromStr};
+use rayon::prelude::*;
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct InputModel {
-    pub pipes: Vec<Pipe>,
+    pub pipes: HashMap<Location, Section>,
 }
 
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
-pub struct Pipe {
-    pub loc: (i64, i64),
-    pub section: Section,
-}
+pub type Location = (i64, i64);
 
-impl Pipe {
-    pub fn valid_next_pipe(&self, next: &Pipe) -> bool {
-        if next.section == Section::Start {
+pub fn valid_next_section(
+    loc: &Location,
+    next: &Location,
+    pipes: &HashMap<Location, Section>,
+) -> bool {
+    if let Some(section) = pipes.get(next) {
+        let section = *section;
+        if section == Section::Start {
             return true;
         }
 
-        let diff = (self.loc.0 - next.loc.0, self.loc.1 - next.loc.1);
+        let diff = (loc.0 - next.0, loc.1 - next.1);
         match diff {
             (0, -1) => {
-                next.section == Section::Vertical
-                    || next.section == Section::CornerNE
-                    || next.section == Section::CornerNW
+                section == Section::Vertical
+                    || section == Section::CornerNE
+                    || section == Section::CornerNW
             }
             (0, 1) => {
-                next.section == Section::Vertical
-                    || next.section == Section::CornerSE
-                    || next.section == Section::CornerSW
+                section == Section::Vertical
+                    || section == Section::CornerSE
+                    || section == Section::CornerSW
             }
             (1, 0) => {
-                next.section == Section::Horizontal
-                    || next.section == Section::CornerNE
-                    || next.section == Section::CornerSE
+                section == Section::Horizontal
+                    || section == Section::CornerNE
+                    || section == Section::CornerSE
             }
             (-1, 0) => {
-                next.section == Section::Horizontal
-                    || next.section == Section::CornerNW
-                    || next.section == Section::CornerSW
+                section == Section::Horizontal
+                    || section == Section::CornerNW
+                    || section == Section::CornerSW
             }
             _ => false,
         }
+    } else {
+        false
     }
+}
 
-    pub fn connecting_pipes(&self, pipes: &[Pipe]) -> Vec<Pipe> {
-        let (x, y) = self.loc;
-        let mut locations = vec![];
-        match self.section {
-            Section::Start => {
-                locations.push((x, y + 1));
-                locations.push((x, y - 1));
-                locations.push((x + 1, y));
-                locations.push((x - 1, y));
-            }
-            Section::Horizontal => {
-                locations.push((x + 1, y));
-                locations.push((x - 1, y));
-            }
-            Section::Vertical => {
-                locations.push((x, y + 1));
-                locations.push((x, y - 1));
-            }
-            Section::CornerNE => {
-                locations.push((x, y - 1));
-                locations.push((x + 1, y));
-            }
-            Section::CornerNW => {
-                locations.push((x, y - 1));
-                locations.push((x - 1, y));
-            }
-            Section::CornerSE => {
-                locations.push((x, y + 1));
-                locations.push((x + 1, y));
-            }
-            Section::CornerSW => {
-                locations.push((x, y + 1));
-                locations.push((x - 1, y));
-            }
+pub fn connecting_pipes((x, y): &Location, pipes: &HashMap<Location, Section>) -> Vec<Location> {
+    let (x, y) = (*x, *y);
+    let section = pipes.get(&(x, y)).unwrap();
+    let mut locations = Vec::with_capacity(4);
+    match section {
+        Section::Start => {
+            locations.push((x, y + 1));
+            locations.push((x, y - 1));
+            locations.push((x + 1, y));
+            locations.push((x - 1, y));
         }
-        locations
-            .iter()
-            .filter_map(|(x, y)| pipes.iter().find(|p| p.loc == (*x, *y)))
-            .filter(|p| self.valid_next_pipe(*p))
-            .cloned()
-            .collect::<Vec<_>>()
+        Section::Horizontal => {
+            locations.push((x + 1, y));
+            locations.push((x - 1, y));
+        }
+        Section::Vertical => {
+            locations.push((x, y + 1));
+            locations.push((x, y - 1));
+        }
+        Section::CornerNE => {
+            locations.push((x, y - 1));
+            locations.push((x + 1, y));
+        }
+        Section::CornerNW => {
+            locations.push((x, y - 1));
+            locations.push((x - 1, y));
+        }
+        Section::CornerSE => {
+            locations.push((x, y + 1));
+            locations.push((x + 1, y));
+        }
+        Section::CornerSW => {
+            locations.push((x, y + 1));
+            locations.push((x - 1, y));
+        }
     }
+    let ls = locations
+        .iter()
+        .filter(|loc| valid_next_section(&(x, y), &loc, &pipes))
+        .cloned()
+        .collect::<Vec<_>>();
+    ls
+}
 
-    pub fn find_path(&self, pipes: &[Pipe], prev_pipe: &Pipe) -> Option<Vec<Pipe>> {
-        iterate((*prev_pipe, *self, vec![*self]), |(last, this, path)| {
-            let next_pipes = this.connecting_pipes(&pipes);
-            let next_pipe = next_pipes
-                .iter()
-                .filter(|&p| *p != *last && !path.contains(p))
-                .cloned()
-                .next();
-            let mut path = path.clone();
-            if let Some(next_pipe) = next_pipe {
-                if !path.contains(&next_pipe) {
-                    path.push(next_pipe);
-                    (*this, next_pipe, path)
-                } else {
-                    (*this, *this, path)
-                }
+pub fn find_path(
+    loc: &Location,
+    prev_pipe: &Location,
+    pipes: &HashMap<Location, Section>,
+) -> Option<Vec<Location>> {
+    let section = pipes.get(loc).unwrap();
+    let mut last = *prev_pipe;
+    let mut loc = *loc;
+    let mut section = *section;
+    let mut path = Vec::<Location>::with_capacity(10000);
+    path.push(loc);
+    let mut finished = false;
+    while (section != Section::Start && !finished) {
+        let next_pipes = connecting_pipes(&loc, &pipes);
+        let next_pipe = next_pipes
+            .iter()
+            .filter(|&l| *l != last && !path.contains(l))
+            .next();
+        if let Some(next_pipe) = next_pipe {
+            if !path.contains(&next_pipe) {
+                path.push(*next_pipe);
+                section = *pipes.get(&next_pipe).unwrap();
+                last = loc;
+                loc = *next_pipe;
             } else {
-                (*this, *this, path)
+                finished = true;
             }
-        })
-        .find(|(last, this, path)| this.section == Section::Start || *last == *this)
-        .map(|(_, _, path)| path)
+        } else {
+            finished = true;
+        }
+    }
+    if finished {
+        None
+    } else {
+        Some(path)
     }
 }
 
@@ -150,40 +169,39 @@ impl FromStr for InputModel {
     type Err = AocError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        s.lines()
+        let lines = s.lines()
             .enumerate()
-            .map(|(y, line)| {
+            .collect::<Vec<_>>();
+
+        lines.par_iter()
+            .flat_map(|(y, line)| {
                 line.chars()
                     .enumerate()
                     .filter(|(_, c)| *c != '.')
-                    .map(|(x, c)| {
+                    .map(move |(x, c)| {
                         let loc = (x as i64, y as i64);
                         let section = Section::from_str(&c.to_string())?;
-                        Ok(Pipe { loc, section })
+                        Ok((loc, section))
                     })
-                    .collect::<Result<Vec<Pipe>, AocError>>()
             })
-            .collect::<Result<Vec<Vec<Pipe>>, AocError>>()
-            .map(|pipes| InputModel {
-                pipes: pipes.into_iter().flatten().collect(),
-            })
+            .collect::<Result<HashMap<Location, Section>, AocError>>()
+            .map(|pipes| InputModel { pipes })
     }
 }
 
-pub fn find_start(pipes: &[Pipe]) -> Pipe {
+pub fn find_start(pipes: &HashMap<Location, Section>) -> Location {
     pipes
         .iter()
-        .find(|p| p.section == Section::Start)
-        .cloned()
+        .find(|(_, section)| **section == Section::Start)
+        .map(|(loc, _)| *loc)
         .unwrap()
 }
 
-pub fn find_loop(pipes: &[Pipe]) -> Option<Vec<Pipe>> {
+pub fn find_loop(pipes: &HashMap<Location, Section>) -> Option<Vec<Location>> {
     let start = find_start(pipes);
-    start
-        .connecting_pipes(pipes)
+    connecting_pipes(&start, pipes)
         .iter()
-        .find_map(|p| p.find_path(pipes, &start))
+        .find_map(|p| find_path(p, &start, pipes))
 }
 
 #[cfg(test)]
@@ -216,39 +234,17 @@ L|-JF";
         let pipes = input.pipes;
 
         let expected = vec![
-            Pipe {
-                loc: (1, 1),
-                section: Section::Start,
-            },
-            Pipe {
-                loc: (2, 1),
-                section: Section::Horizontal,
-            },
-            Pipe {
-                loc: (3, 1),
-                section: Section::CornerSW,
-            },
-            Pipe {
-                loc: (1, 2),
-                section: Section::Vertical,
-            },
-            Pipe {
-                loc: (3, 2),
-                section: Section::Vertical,
-            },
-            Pipe {
-                loc: (1, 3),
-                section: Section::CornerNE,
-            },
-            Pipe {
-                loc: (2, 3),
-                section: Section::Horizontal,
-            },
-            Pipe {
-                loc: (3, 3),
-                section: Section::CornerNW,
-            },
-        ];
+            ((1, 1), Section::Start),
+            ((2, 1), Section::Horizontal),
+            ((3, 1), Section::CornerSW),
+            ((1, 2), Section::Vertical),
+            ((3, 2), Section::Vertical),
+            ((1, 3), Section::CornerNE),
+            ((2, 3), Section::Horizontal),
+            ((3, 3), Section::CornerNW),
+        ]
+        .into_iter()
+        .collect::<HashMap<_, _>>();
         assert_eq!(pipes, expected);
     }
 
@@ -256,38 +252,40 @@ L|-JF";
     fn test_connecting_sections() {
         let input = InputModel::from_str(TEXT_INPUT).unwrap();
         let pipes = input.pipes;
+        let mut locations = pipes.keys().cloned().collect::<Vec<_>>();
+        locations.sort();
 
-        let mut cs = pipes[0].connecting_pipes(&pipes);
+        let mut cs = connecting_pipes(&locations[0], &pipes);
         cs.sort();
-        assert_eq!(cs, vec![pipes[3], pipes[1]]);
+        assert_eq!(cs, vec![locations[1], locations[3]]);
 
-        let mut cs = pipes[1].connecting_pipes(&pipes);
+        let mut cs = connecting_pipes(&locations[1], &pipes);
         cs.sort();
-        assert_eq!(cs, vec![pipes[0], pipes[2]]);
+        assert_eq!(cs, vec![locations[0], locations[2]]);
 
-        let mut cs = pipes[2].connecting_pipes(&pipes);
+        let mut cs = connecting_pipes(&locations[2], &pipes);
         cs.sort();
-        assert_eq!(cs, vec![pipes[1], pipes[4]]);
+        assert_eq!(cs, vec![locations[1], locations[4]]);
 
-        let mut cs = pipes[3].connecting_pipes(&pipes);
+        let mut cs = connecting_pipes(&locations[3], &pipes);
         cs.sort();
-        assert_eq!(cs, vec![pipes[0], pipes[5]]);
+        assert_eq!(cs, vec![locations[0], locations[5]]);
 
-        let mut cs = pipes[4].connecting_pipes(&pipes);
+        let mut cs = connecting_pipes(&locations[4], &pipes);
         cs.sort();
-        assert_eq!(cs, vec![pipes[2], pipes[7]]);
+        assert_eq!(cs, vec![locations[2], locations[7]]);
 
-        let mut cs = pipes[5].connecting_pipes(&pipes);
+        let mut cs = connecting_pipes(&locations[5], &pipes);
         cs.sort();
-        assert_eq!(cs, vec![pipes[3], pipes[6]]);
+        assert_eq!(cs, vec![locations[3], locations[6]]);
 
-        let mut cs = pipes[6].connecting_pipes(&pipes);
+        let mut cs = connecting_pipes(&locations[6], &pipes);
         cs.sort();
-        assert_eq!(cs, vec![pipes[5], pipes[7]]);
+        assert_eq!(cs, vec![locations[5], locations[7]]);
 
-        let mut cs = pipes[7].connecting_pipes(&pipes);
+        let mut cs = connecting_pipes(&locations[7], &pipes);
         cs.sort();
-        assert_eq!(cs, vec![pipes[6], pipes[4]]);
+        assert_eq!(cs, vec![locations[4], locations[6]]);
     }
 
     #[test]
@@ -295,69 +293,83 @@ L|-JF";
         let input = InputModel::from_str(TEXT_INPUT).unwrap();
         let pipes = input.pipes;
         let start = find_start(&pipes);
-        assert_eq!(start.loc, (1, 1));
+        assert_eq!(start, (1, 1));
     }
 
     #[test]
-    fn test_valid_next_pipe() {
+    fn test_valid_next_section() {
         let input = InputModel::from_str(TEXT_INPUT).unwrap();
         let pipes = input.pipes;
-        assert!(pipes[0].valid_next_pipe(&pipes[1]));
-        assert!(pipes[0].valid_next_pipe(&pipes[3]));
+        let mut locations = pipes.keys().cloned().collect::<Vec<_>>();
+        locations.sort();
 
-        assert!(pipes[1].valid_next_pipe(&pipes[0]));
-        assert!(pipes[1].valid_next_pipe(&pipes[2]));
+        assert!(valid_next_section(&locations[0], &locations[1], &pipes));
+        assert!(valid_next_section(&locations[0], &locations[3], &pipes));
 
-        assert!(pipes[2].valid_next_pipe(&pipes[1]));
-        assert!(pipes[2].valid_next_pipe(&pipes[4]));
+        assert!(valid_next_section(&locations[1], &locations[0], &pipes));
+        assert!(valid_next_section(&locations[1], &locations[2], &pipes));
 
-        assert!(pipes[3].valid_next_pipe(&pipes[0]));
-        assert!(pipes[3].valid_next_pipe(&pipes[5]));
+        assert!(valid_next_section(&locations[2], &locations[1], &pipes));
+        assert!(valid_next_section(&locations[2], &locations[4], &pipes));
 
-        assert!(pipes[4].valid_next_pipe(&pipes[2]));
-        assert!(pipes[4].valid_next_pipe(&pipes[7]));
+        assert!(valid_next_section(&locations[3], &locations[0], &pipes));
+        assert!(valid_next_section(&locations[3], &locations[5], &pipes));
 
-        assert!(pipes[5].valid_next_pipe(&pipes[3]));
+        assert!(valid_next_section(&locations[4], &locations[2], &pipes));
+        assert!(valid_next_section(&locations[4], &locations[7], &pipes));
 
-        assert!(pipes[6].valid_next_pipe(&pipes[5]));
-        assert!(pipes[6].valid_next_pipe(&pipes[7]));
+        assert!(valid_next_section(&locations[5], &locations[3], &pipes));
 
-        assert!(pipes[7].valid_next_pipe(&pipes[6]));
-        assert!(pipes[7].valid_next_pipe(&pipes[4]));
+        assert!(valid_next_section(&locations[6], &locations[5], &pipes));
+        assert!(valid_next_section(&locations[6], &locations[7], &pipes));
+
+        assert!(valid_next_section(&locations[7], &locations[6], &pipes));
+        assert!(valid_next_section(&locations[7], &locations[4], &pipes));
     }
 
     #[test]
     fn test_find_path() {
         let input = InputModel::from_str(TEXT_INPUT).unwrap();
         let pipes = input.pipes;
-        let path = pipes[1].find_path(&pipes, &pipes[0]);
+        let mut locations = pipes.keys().cloned().collect::<Vec<_>>();
+        locations.sort();
+
+        let start = find_start(&pipes);
+        let next_locs = connecting_pipes(&start, &pipes);
+        let next = next_locs.first().unwrap();
+        let path = find_path(next, &start, &pipes);
         assert_eq!(
             path,
             Some(vec![
-                pipes[1], pipes[2], pipes[4], pipes[7], pipes[6], pipes[5], pipes[3], pipes[0]
+                locations[1],
+                locations[2],
+                locations[4],
+                locations[7],
+                locations[6],
+                locations[5],
+                locations[3],
+                locations[0]
             ])
         );
 
         let input = InputModel::from_str(TEXT_INPUT_2).unwrap();
         let pipes = input.pipes;
         let start = find_start(&pipes);
-        let next_pipes = start.connecting_pipes(&pipes);
-        println!("{:?}", next_pipes);
+        let next_pipes = connecting_pipes(&start, &pipes);
         assert_eq!(next_pipes.len(), 2);
-        let path = next_pipes[0].find_path(&pipes, &start).unwrap();
+        let path = find_path(&next_pipes[0], &start, &pipes).unwrap();
         assert_eq!(path.len(), 16);
-        let path = next_pipes[1].find_path(&pipes, &start).unwrap();
+        let path = find_path(&next_pipes[1], &start, &pipes).unwrap();
         assert_eq!(path.len(), 16);
 
         let input = InputModel::from_str(TEXT_INPUT_2).unwrap();
         let pipes = input.pipes;
         let start = find_start(&pipes);
-        let next_pipes = start.connecting_pipes(&pipes);
-        println!("{:?}", next_pipes);
+        let next_pipes = connecting_pipes(&start, &pipes);
         assert_eq!(next_pipes.len(), 2);
-        let path = next_pipes[0].find_path(&pipes, &start).unwrap();
+        let path = find_path(&next_pipes[0], &start, &pipes).unwrap();
         assert_eq!(path.len(), 16);
-        let path = next_pipes[1].find_path(&pipes, &start).unwrap();
+        let path = find_path(&next_pipes[1], &start, &pipes).unwrap();
         assert_eq!(path.len(), 16);
     }
 
