dd502189f49fe8d1c3b0cc7b13e295bd83da74a7
Peter Tillemans <pti@melexis.com>
2023-12-19 13:23:14 +0100

    solved day 19

---
+++ aoc-2023-19/src/main.rs
@@ -0,0 +1,106 @@
+#![feature(test)]
+use aoc_2023_19::model::{Action, AocError, PartRange};
+use aoc_2023_19::InputModel;
+use rayon::prelude::*;
+
+const INPUT: &str = include_str!("../data/input.txt");
+
+fn part1(input: &InputModel) -> Result<String, AocError> {
+    let wfs = &input.workflows;
+    Ok(input
+        .parts
+        .par_iter()
+        .filter(|p| wfs.evaluate(p) == Ok(Action::Accepted))
+        .map(|p| p.rating())
+        .sum::<u64>()
+        .to_string())
+}
+
+fn part2(input: &InputModel) -> Result<String, AocError> {
+    let start = PartRange::default();
+    let wfs = &input.workflows;
+    let result = wfs.evaluate_range(&start)?;
+    Ok(result.iter()
+        .map(|p| p.combinations())
+        .sum::<u64>().to_string())
+}
+
+fn main() -> Result<(), AocError> {
+    let input: InputModel = INPUT.parse::<InputModel>()?;
+    let part1_result = part1(&input)?;
+    println!("Part1: {}", part1_result);
+    println!("--------------");
+    let part2_result = part2(&input)?;
+    println!("Part2: {}", part2_result);
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    extern crate test;
+    use super::*;
+    use test::Bencher;
+
+    const TEST_INPUT: &str = "px{a<2006:qkq,m>2090:A,rfg}
+pv{a>1716:R,A}
+lnx{m>1548:A,A}
+rfg{s<537:gd,x>2440:R,A}
+qs{s>3448:A,lnx}
+qkq{x<1416:A,crn}
+crn{x>2662:A,R}
+in{s<1351:px,qqz}
+qqz{s>2770:qs,m<1801:hdj,R}
+gd{a>3333:R,R}
+hdj{m>838:A,pv}
+
+{x=787,m=2655,a=1222,s=2876}
+{x=1679,m=44,a=2067,s=496}
+{x=2036,m=264,a=79,s=2244}
+{x=2461,m=1339,a=466,s=291}
+{x=2127,m=1623,a=2188,s=1013}";
+
+    pub fn input_data() -> InputModel {
+        TEST_INPUT.parse::<InputModel>().unwrap()
+    }
+
+    #[test]
+    fn test_parse() {
+        let actual = TEST_INPUT.parse::<InputModel>().unwrap();
+        let expected = input_data();
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_part1() {
+        let actual = part1(&input_data()).unwrap();
+        let expected = "19114";
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_part2() {
+        let actual = part2(&input_data()).unwrap();
+        let expected = "167409079868000";
+
+        assert_eq!(actual, expected);
+    }
+
+    #[bench]
+    fn bench_parse(b: &mut Bencher) {
+        b.iter(|| INPUT.parse::<InputModel>().unwrap())
+    }
+
+    #[bench]
+    fn bench_part1(b: &mut Bencher) {
+        let data = INPUT.parse::<InputModel>().unwrap();
+        b.iter(|| part1(&data))
+    }
+
+    #[bench]
+    fn bench_part2(b: &mut Bencher) {
+        let data = INPUT.parse::<InputModel>().unwrap();
+        b.iter(|| part2(&data))
+    }
+}
