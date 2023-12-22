7dc770726cacf3beb00040072e31d4b596e5b894
Peter Tillemans <pti@melexis.com>
2023-12-22 13:11:48 +0100

    solved day 22

---
+++ aoc-2023-22/src/main.rs
@@ -0,0 +1,86 @@
+#![feature(test)]
+use aoc_2023_22::{AocError, BrickPile, InputModel};
+
+const INPUT: &str = include_str!("../data/input.txt");
+
+fn part1(input: &InputModel) -> Result<String, AocError> {
+    let pile = BrickPile::new(&input.bricks);
+    let disintegrable = pile.disintegrable_bricks();
+    let result = disintegrable.len();
+    Ok(result.to_string())
+}
+
+fn part2(input: &InputModel) -> Result<String, AocError> {
+    let pile = BrickPile::new(&input.bricks);
+    Ok(pile.chain_reaction_sum().to_string())
+}
+
+fn main() -> Result<(), AocError> {
+    let input: InputModel = INPUT.parse::<InputModel>()?;
+    let part1_result = part1(&input)?;
+    println!("Part1: {}", part1_result);
+    println!("--------------");
+    let part2_result = part2(&input)?;
+    println!("Part2: {}", part2_result);
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    extern crate test;
+    use super::*;
+    use test::Bencher;
+
+    const TEST_INPUT: &str = "1,0,1~1,2,1
+0,0,2~2,0,2
+0,2,3~2,2,3
+0,0,4~0,2,4
+2,0,5~2,2,5
+0,1,6~2,1,6
+1,1,8~1,1,9";
+
+    pub fn input_data() -> InputModel {
+        TEST_INPUT.parse::<InputModel>().unwrap()
+    }
+
+    #[test]
+    fn test_parse() {
+        let actual = TEST_INPUT.parse::<InputModel>().unwrap();
+        let expected = input_data();
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_part1() {
+        let actual = part1(&input_data()).unwrap();
+        let expected = "5";
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_part2() {
+        let actual = part2(&input_data()).unwrap();
+        let expected = "7";
+
+        assert_eq!(actual, expected);
+    }
+
+    #[bench]
+    fn bench_parse(b: &mut Bencher) {
+        b.iter(|| INPUT.parse::<InputModel>().unwrap())
+    }
+
+    #[bench]
+    fn bench_part1(b: &mut Bencher) {
+        let data = INPUT.parse::<InputModel>().unwrap();
+        b.iter(|| part1(&data))
+    }
+
+    #[bench]
+    fn bench_part2(b: &mut Bencher) {
+        let data = INPUT.parse::<InputModel>().unwrap();
+        b.iter(|| part2(&data))
+    }
+}
